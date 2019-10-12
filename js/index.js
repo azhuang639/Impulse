c0112463af1a9c4b729e7410ee932fd8441abcb6
Alan Bi <alan.bi326@gmail.com>
2019-10-12 14:23:22 -0400

    Add insertion code

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-console.log('Hello, world! From Impulse');
+insertWindow();
+
+function insertWindow() {
+  fetch(chrome.extension.getURL('html/window.html'))
+    .then(response => response.text())
+    .then(data => {
+      const body = document.getElementsByTagName('body')[0];
+      const window = document.createElement('div');
+      window.innerHTML = data;
+      body.append(window);
+
+    }).catch(err => {
+    console.log('Error loading Impulse window:' + err)
+  });
+}
