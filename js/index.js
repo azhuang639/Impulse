621bb440ba956836dfbede93b425aa9c29c63bc5
Alan Bi <alan.bi326@gmail.com>
2019-10-12 15:07:14 -0400

    Get window and overlay working

--- js/index.js
+++ js/index.js
@@ -1,4 +1,25 @@
-insertWindow();
+trackImpulse();
+
+function trackImpulse() {
+  let targetButton;
+  const url = location.href;
+  if (!url) {
+    return;
+  }
+
+  if (url.includes('amazon.com')) {
+    targetButton = document.getElementById('add-to-cart-button');
+  } else if (url.includes('ebay.com')) {
+    targetButton = null; // replace with the id of the button we want to track
+  }
+
+  if (targetButton) {
+    targetButton.addEventListener('click', (e) => {
+      e.preventDefault();
+      insertWindow();
+    });
+  }
+}
 
 function insertWindow() {
   fetch(chrome.extension.getURL('html/window.html'))
@@ -6,8 +27,13 @@ function insertWindow() {
     .then(data => {
       const body = document.getElementsByTagName('body')[0];
       const window = document.createElement('div');
+      window.className = 'impulse-window';
       window.innerHTML = data;
       body.append(window);
+
+      const overlay = document.createElement('div');
+      overlay.className = 'impulse-overlay';
+      body.append(overlay);
 
     }).catch(err => {
     console.log('Error loading Impulse window:' + err)
