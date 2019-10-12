57b8a5183ae76f3fa495a531053c47d7d931e592
William Luqiu <william.luqiu@duke.edu>
2019-10-12 15:05:03 -0400

    added more buttons

--- js/window.js
+++ js/window.js
@@ -1,3 +1,39 @@
-function myFunction(){
-    console.log('test')
+var bought = 0;
+var not_bought = 0;
+
+window.onload = startup;
+
+function startup(){
+    console.log("STARTUP");
+    document.getElementById("close").style.display = "none";
 }
+function didBuy(){
+    bought++;
+    console.log('test');
+    document.getElementById("failure?").innerHTML = "wow, you really succumbed to your impulses. what. a. failure.";
+    appear();
+    //document.getElementById("close").innerHTML = "close window";
+}
+
+function didNotBuy(){
+    not_bought++;
+    console.log('didn\'t buy item');
+    document.getElementById("failure?").innerHTML = "congrats, you stood up to your impulses!";
+    appear();
+    //document.getElementById("close").innerHTML = "close window";
+   // window.close();
+}
+function closewindow(){
+    console.log("test");
+    window.close();
+}
+
+function appear() {
+    var x = document.getElementById("close");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
