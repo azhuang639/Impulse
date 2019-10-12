471f4ec98ff0f1e6c6f43cde0facea6cf75ea2e7
Alex Zhuang <azhuang639@gmail.com>
2019-10-12 17:17:59 -0400

    pause and resume changes

--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,32 @@
 console.log('Popup opened');
+
+var active = true;
+
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('pause');
+    link.addEventListener("load", function() {
+        loading(link);
+    });
+    link.addEventListener('click', function() {
+        reverse(link);
+    });
+});
+
+function reverse(element) {
+    if (active) {
+        element.className = "resume";
+        element.innerHTML = "Resume Impulse";
+        active = false;
+    } else {
+        element.className = "pause";
+        element.innerHTML = "Pause Impulse";
+        active = true;
+    }
+}
+
+function loading(element) {
+    if (!active) {
+        element.className = "resume";
+        element.innerHTML = "Resume Impulse";
+    }
+}
